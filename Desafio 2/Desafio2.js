6bb66dd41c23c48c27ce05ecb23d4457b9e48671
Martina Rodriguez Arona <tini.arona16@gmail.com>
2024-09-21 20:49:57 -1200

    fue colocado lo que se pidio en la actividad 2

---
+++ Desafio 2/Desafio2.js
@@ -0,0 +1,20 @@
+
+const nombreProducto = "PC" 
+
+let X = 3
+
+const precioUnitario = 3684920
+
+let E = prompt('ingrese la cantidad deseada: ')
+
+let CantidadDeseada = console.log(E+" PC de $3684920")
+
+alert("Costo total de su compra: $"+precioUnitario*E )
+
+
+
+
+
+
+
+
